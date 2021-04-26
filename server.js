425bfaf874480c999c41e92c8a9a862ed3eefd0e
MiseryD <80783487+MiseryD@users.noreply.github.com>
2021-04-26 13:27:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ client.on("message", async message => {
 client.on('guildMemberAdd', (member) => {
   //Lorsqu'un Utilisateur Rejoint.
   let welcomeChannel = client.channels.cache.get('825362499934617620');
-  welcomeChannel.send('**Au revoir**, <@ + member.user.id + > ! 👋');
+  welcomeChannel.send('**Au revoir**, <@' + member.user.id + '> ! 👋');
 
   member.roles.add('834857843525812228');
   member.roles.add('826032153141051392');
@@ -97,8 +97,7 @@ client.on('guildMemberAdd', (member) => {
 client.on('guildMemberRemove', (member) => {
   //Lorsqu'un utilisateur Quitte.
   let leaveChannel = client.channels.cache.get('825362499934617620'); 
-  leaveChannel.send('Au revoir, <@ + member.user.id + >
- ! 🙂');
+  leaveChannel.send('Au revoir, <@' + member.user.id + '> ! 🙂');
 });
 
 client.login(process.env.TOKEN);
