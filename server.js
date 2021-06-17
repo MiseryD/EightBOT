6840db15d7830ac66f1f973ec71319f2ee149c36
MiseryD <80783487+MiseryD@users.noreply.github.com>
2021-06-17 22:05:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -84,19 +84,15 @@ client.on("message", async message => {
 
 client.on('guildMemberAdd', (member) => {
   //Lorsqu'un Utilisateur Rejoint.
-  let welcomeChannel = client.channels.cache.get('855154973125640192');
-  
+  let welcomeChannel = client.channels.cache.get('825362499934617620');
   welcomeChannel.send('**Bienvenue** <@' + member.user.tag + '> ! 👋');
-
-  member.roles.add('855153584735518770');
 
   member.send('**Bienvenue** sur le serveur **__MiseryCord__** ! Lis attentivement les règles et coche le case en dessous pour avoir accès au serveur ! Si tu rencontre un problème contacte <@541992802146451476> en messages privés 😉');
 });
 
 client.on('guildMemberRemove', (member) => {
   //Lorsqu'un utilisateur Quitte.
-  let leaveChannel = client.channels.cache.get('855154973125640192');
-  
+  let leaveChannel = client.channels.cache.get('825362499934617620');
   leaveChannel.send('Au revoir, <@' + member.user.tag + '> ! 🙂');
 });
 
